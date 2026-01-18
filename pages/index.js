85b47d6a13dd14dfe1ae85dea7479dd2b0634c70
Donation-website <lipcseitundetimea@gmail.com>
2026-01-18 13:30:51 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -10,10 +10,6 @@ export default function Home() {
     <>
       <Head>
         <title>Lunar Pre-Emptive Rights</title>
-        <meta
-          name="description"
-          content="Secure a speculative pre-emptive position on the Moon. A future-facing conceptual initiative."
-        />
       </Head>
 
       <main style={{ width: "100vw", height: "100vh", background: "black" }}>
