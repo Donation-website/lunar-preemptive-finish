c340f2eb0f9755cc8395cb3c2a44a64e7b7401ab
Donation-website <lipcseitundetimea@gmail.com>
2026-01-18 14:56:37 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,13 @@
 import dynamic from 'next/dynamic'
+import Header from '../components/Header'
 
 const MoonScene = dynamic(() => import('../components/MoonScene'), { ssr: false })
 
 export default function Home() {
   return (
-    <div>
-      <h1>🌕 Lunar Pre-Emptive Rights – LIVE</h1>
+    <>
+      <Header />
       <MoonScene />
-
-      <div className="aszf-container">
-        <details>
-          <summary>ÁSZF</summary>
-          <p>Ez a szolgáltatás spekulatív és nem biztosít tulajdonjogot a Holdon. A vásárlásodat dokumentáltan rögzítjük a jövőbeli jogszabályi változások reményében.</p>
-        </details>
-      </div>
-
-      <div className="social-icons">
-        <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">FB</a>
-        <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">IG</a>
-        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">X</a>
-      </div>
-    </div>
+    </>
   )
 }
