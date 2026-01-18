7365f8e7f8334e92648f3a1d959d1734ad73e5b9
Donation-website <lipcseitundetimea@gmail.com>
2026-01-18 13:23:35 +0100

    Create MoonScene.js

---
+++ components/MoonScene.js
@@ -0,0 +1,40 @@
+import { Canvas, useFrame } from "@react-three/fiber";
+import { OrbitControls, Stars } from "@react-three/drei";
+import * as THREE from "three";
+import { useRef } from "react";
+
+function Moon() {
+  const meshRef = useRef();
+
+  useFrame(() => {
+    meshRef.current.rotation.y += 0.0008;
+  });
+
+  return (
+    <mesh ref={meshRef}>
+      <sphereGeometry args={[2.5, 64, 64]} />
+      <meshStandardMaterial
+        color="#bfbfbf"
+        roughness={1}
+        metalness={0}
+      />
+    </mesh>
+  );
+}
+
+export default function MoonScene() {
+  return (
+    <Canvas camera={{ position: [0, 0, 7], fov: 50 }}>
+      <ambientLight intensity={0.4} />
+      <directionalLight position={[5, 5, 5]} intensity={1.2} />
+      <Stars radius={100} depth={50} count={5000} factor={4} fade />
+      <Moon />
+      <OrbitControls
+        enableZoom={true}
+        enablePan={false}
+        minDistance={4}
+        maxDistance={10}
+      />
+    </Canvas>
+  );
+}
