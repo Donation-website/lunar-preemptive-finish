aa72b8410930eb937bda42f83b44217e48cc8747
Donation-website <lipcseitundetimea@gmail.com>
2026-01-21 06:39:58 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import dynamic from 'next/dynamic'
 
 const MoonScene = dynamic(() => import('../components/MoonScene'), {
@@ -6,41 +5,5 @@ const MoonScene = dynamic(() => import('../components/MoonScene'), {
 })
 
 export default function Home() {
-  const [selected, setSelected] = useState(null)
-
-  return (
-    <div>
-      <h1 className="title">🌕 Lunar Pre-Emptive Rights</h1>
-
-      <MoonScene onParcelSelect={setSelected} />
-
-      {selected && (
-        <div className="modal">
-          <h2>Parcel #{selected.id}</h2>
-          <p>Price: ${selected.price}</p>
-          <p>
-            Coordinates:<br />
-            x: {selected.position.x.toFixed(2)}<br />
-            y: {selected.position.y.toFixed(2)}<br />
-            z: {selected.position.z.toFixed(2)}
-          </p>
-
-          <a
-            href={`/checkout?parcel=${selected.id}`}
-            className="buy"
-          >
-            Proceed to Checkout
-          </a>
-
-          <button onClick={() => setSelected(null)}>Close</button>
-        </div>
-      )}
-
-      <footer>
-        <a href="https://facebook.com" target="_blank">Facebook</a>
-        <a href="https://instagram.com" target="_blank">Instagram</a>
-        <a href="https://x.com" target="_blank">X</a>
-      </footer>
-    </div>
-  )
+  return <MoonScene />
 }
