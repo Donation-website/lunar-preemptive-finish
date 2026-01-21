a519e1764b790cd5a89088d0b4f7d5fc0a5b84f3
Donation-website <lipcseitundetimea@gmail.com>
2026-01-21 06:09:43 +0100

    Update MoonScene.js

--- components/MoonScene.js
+++ components/MoonScene.js
@@ -1,5 +1,5 @@
 import { Canvas, useFrame } from '@react-three/fiber'
-import { OrbitControls, Sphere } from '@react-three/drei'
+import { OrbitControls } from '@react-three/drei'
 import * as THREE from 'three'
 import { useRef, useMemo } from 'react'
 
@@ -7,9 +7,7 @@ function Moon() {
   const ref = useRef()
 
   useFrame(() => {
-    if (ref.current) {
-      ref.current.rotation.y += 0.001
-    }
+    if (ref.current) ref.current.rotation.y += 0.001
   })
 
   return (
@@ -22,44 +20,49 @@ function Moon() {
   )
 }
 
-function Parcels() {
+function Parcels({ onSelect }) {
   const parcels = useMemo(() => {
-    return Array.from({ length: 180 }).map(() => {
+    return Array.from({ length: 180 }).map((_, i) => {
       const theta = Math.random() * Math.PI * 2
       const phi = Math.acos(2 * Math.random() - 1)
       const r = 2.01
 
-      return new THREE.Vector3(
-        r * Math.sin(phi) * Math.cos(theta),
-        r * Math.cos(phi),
-        r * Math.sin(phi) * Math.sin(theta)
-      )
+      return {
+        id: i + 1,
+        price: 990,
+        position: new THREE.Vector3(
+          r * Math.sin(phi) * Math.cos(theta),
+          r * Math.cos(phi),
+          r * Math.sin(phi) * Math.sin(theta)
+        )
+      }
     })
   }, [])
 
-  return parcels.map((pos, i) => (
-    <mesh key={i} position={pos}>
+  return parcels.map(parcel => (
+    <mesh
+      key={parcel.id}
+      position={parcel.position}
+      onClick={() => onSelect(parcel)}
+    >
       <circleGeometry args={[0.05, 32]} />
       <meshBasicMaterial
         color="lime"
         transparent
-        opacity={0.8}
+        opacity={0.9}
         side={THREE.DoubleSide}
       />
     </mesh>
   ))
 }
 
-export default function MoonScene() {
+export default function MoonScene({ onParcelSelect }) {
   return (
-    <Canvas
-      camera={{ position: [0, 0, 6], fov: 50 }}
-      style={{ width: '100vw', height: '100vh' }}
-    >
+    <Canvas camera={{ position: [0, 0, 6], fov: 50 }}>
       <ambientLight intensity={0.6} />
       <directionalLight position={[5, 5, 5]} intensity={1} />
       <Moon />
-      <Parcels />
+      <Parcels onSelect={onParcelSelect} />
       <OrbitControls enableZoom enableRotate />
     </Canvas>
   )
